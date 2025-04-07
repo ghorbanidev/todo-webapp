8947b9b625d9465d9a21ddf6d504fefb43f64645
Ali Ghorbani <119003410+ghorbanidev@users.noreply.github.com>
2025-04-07 11:58:08 +0330

    complete

---
+++ src/components/shared/icons/index.tsx
@@ -0,0 +1,8 @@
+export { default as ArrowLeftIcon } from './src/ArrowLeftIcon';
+export { default as ComputerIcon } from './src/ComputerIcon';
+export { default as DeleteIcon } from './src/DeleteIcon';
+export { default as EditIcon } from './src/EditIcon';
+export { default as MoonIcon } from './src/MoonIcon';
+export { default as SunIcon } from './src/SunIcon';
+export { default as TickIcon } from './src/TickIcon';
+export { default as TwoArrowLeftIcon } from './src/TwoArrowLeftIcon';
